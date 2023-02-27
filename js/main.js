ab54c961a6032ce60eca33ed1d6d97c6e18b5db1
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-27 23:04:55 +0100

    step 1 completato

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-'use strict';
+// 'use strict';
+
+// const imgs = [
+//     'slide1.svg',
+//     'slide2.svg',
+//     'slide3.svg',
+//     'slide4.svg',
+//     'slide5.svg'
+// ]
+
+// let itemContent = '';
+
+// const slider = document.querySelector('.slider');
+
+// for(let i=0; i < imgs.length; i++){
+//     itemContent += `<div class="item"><img src="img/slide1.svg" alt=""></div>`;
+// }
+
+// slider.innerHTML += itemContent;
+// document.querySelector('.item').classList.add('show');
