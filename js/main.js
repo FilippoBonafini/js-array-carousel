88490c08e4bfeb3db1c2d0277e7d40c10b7ca835
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-28 00:37:39 +0100

    bonus 2

--- js/main.js
+++ js/main.js
@@ -9,10 +9,13 @@ const imgs = [
 ]
 
 let itemContent = '';
+let prevContent = '';
 
 let active = 0;
 
 const slider = document.querySelector('.slider');
+
+
 
 for(let i=0; i < imgs.length; i++){
     itemContent += `<div class="item"><img src="img/${imgs[i]}" alt=""></div>`;
@@ -21,32 +24,68 @@ for(let i=0; i < imgs.length; i++){
 slider.innerHTML += itemContent;
 document.querySelector('.item').classList.add('show');
 
+
+
+
+
+
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 const items = document.querySelectorAll('.item');
+
+
+
+const preview = document.querySelector('.preview');
+
+
+
+for(let i=0; i < imgs.length; i++){
+    prevContent += `<div class="prevItem hidden"><img src="img/${imgs[i]}" alt=""></div>`;
+}
+preview.innerHTML += prevContent;
+
+const previewList = document.querySelectorAll('.prevItem');
+
+
+document.querySelector('.prevItem').classList.remove('hidden');
+
+
+console.log(previewList)
 
 prev.addEventListener('click',
 function(){
     if(active > 0 ){
         items[active].classList.remove('show');
+        previewList[active].classList.add('hidden');
         active--;
-        items[active].classList.add('show')
+        items[active].classList.add('show');
+        previewList[active].classList.remove('hidden');
     } else{
         items[active].classList.remove('show');
+        previewList[active].classList.add('hidden');
         active = items.length - 1;
-        items[active].classList.add('show')
+        items[active].classList.add('show');   
+        previewList[active].classList.remove('hidden');
     }
+
 })
 
 next.addEventListener('click',
 function(){
     if(active < items.length - 1){
         items[active].classList.remove('show');
+        previewList[active].classList.add('hidden');
         active++;
-        items[active].classList.add('show')
+        previewList[active].classList.remove('hidden');
+        items[active].classList.add('show');
     }else{
         items[active].classList.remove('show');
+        previewList[active].classList.add('hidden');
         active = 0;
-        items[active].classList.add('show')
+        items[active].classList.add('show');
+        previewList[active].classList.remove('hidden');
     }
 })
+
+
+
