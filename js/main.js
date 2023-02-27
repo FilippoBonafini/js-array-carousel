7a96304610349171794bf926b5b4046ffcf55e97
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-27 23:13:36 +0100

    Step 2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
-// 'use strict';
+'use strict';
 
-// const imgs = [
-//     'slide1.svg',
-//     'slide2.svg',
-//     'slide3.svg',
-//     'slide4.svg',
-//     'slide5.svg'
-// ]
+const imgs = [
+    'slide1.svg',
+    'slide2.svg',
+    'slide3.svg',
+    'slide4.svg',
+    'slide5.svg'
+]
 
-// let itemContent = '';
+let itemContent = '';
 
-// const slider = document.querySelector('.slider');
+let active = 0;
 
-// for(let i=0; i < imgs.length; i++){
-//     itemContent += `<div class="item"><img src="img/slide1.svg" alt=""></div>`;
-// }
+const slider = document.querySelector('.slider');
 
-// slider.innerHTML += itemContent;
-// document.querySelector('.item').classList.add('show');
+for(let i=0; i < imgs.length; i++){
+    itemContent += `<div class="item"><img src="img/slide1.svg" alt=""></div>`;
+}
+
+slider.innerHTML += itemContent;
+document.querySelector('.item').classList.add('show');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+prev.addEventListener('click',
+function(){
+    alert('prev')
+})
+
+next.addEventListener('click',
+function(){
+    alert('next')
+    
+})
